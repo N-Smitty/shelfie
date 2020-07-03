cd9992cf97f55bdaceee49f2bb7ff882df8cf42e
Natalie Gutierrez-Hill Smith <nataliesmith@MacBook-Pro.local>
2020-07-02 23:09:39 -0600

    progress

---
+++ shelfie.cd/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Dashboard from '../Dashboard/Dashboard';
+import Form from '../Form/Form';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Dashboard/>
+      <Form/>
+    </div>
+  )
+}
+
+export default App;
